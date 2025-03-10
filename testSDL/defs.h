24ac0ae078f2d530ad50045d3c9a3615016d5817
tangerinebetter <hieubeo264@gmail.com>
2025-03-10 22:02:54 +0700

    test chia file

---
+++ testSDL/defs.h
@@ -0,0 +1,8 @@
+#ifndef DEFS_H_INCLUDED
+#define DEFS_H_INCLUDED
+
+const int SCREEN_WIDTH = 600;
+const int SCREEN_HEIGHT = 900;
+const char* WINDOW_TITLE = "Air Hockey";
+
+#endif // DEFS_H_INCLUDED
